54b247bae325ca4478b3d4cd6e21cd28914e0857
TKMT <tkmt.takashi02@gmail.com>
2024-03-13 05:37:18 +0900

    コメント入力のコンポーネント

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -14,6 +14,7 @@ import Comment from '@/components/comment';
 import UserInfo from '@/components/user_info';
 import Hobbys from '@/components/hobby';
 import PostList from '@/components/postList';
+import CommentInput from '@/components/comment_input';
 
 
 
@@ -35,6 +36,7 @@ export default function App() {
             <UserInfo />
             <Hobbys />
             <PostList />
+            <CommentInput />
         </>
     )
 }
