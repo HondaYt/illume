261561a46ad68fdb3f85ec4613963cb2d179b777
TKMT <tkmt.takashi02@gmail.com>
2024-03-12 17:08:47 +0900

    スプラッシュ画面の作成

---
+++ src/app/main/page.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+
+const HomePage = () => {
+    return (
+    <div>
+        <h1>Welcome to the Home Page!</h1>
+        {/* 他のコンテンツを追加できます */}
+    </div>
+    );
+};
+
+export default HomePage;
