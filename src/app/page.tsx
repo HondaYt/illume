30925f857e4904ac11771bfd620c2fcc5cabdf04
TKMT <tkmt.takashi02@gmail.com>
2024-03-12 17:23:38 +0900

    スプラッシュの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 "use client"
-import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+
 
 
 const HomePage = () => {
     const Router = useRouter();
 
-    useEffect(() => {
-    // 2秒後に次のページに遷移
-    const timeoutId = setTimeout(() => {
-        Router.push('/main');
-    }, 2000);
+    // useEffect(() => {
+    // // 2秒後に次のページに遷移
+    // const timeoutId = setTimeout(() => {
+    //     Router.push('/main');
+    // }, 2000);
 
-    // コンポーネントがアンマウントされた場合にクリーンアップ
-    return () => clearTimeout(timeoutId);
-    }, [Router]);
+    // // コンポーネントがアンマウントされた場合にクリーンアップ
+    // return () => clearTimeout(timeoutId);
+    // }, [Router]);
 
     return (
-    <div className='mainvisual'>
-        <img src="/public/next.svg" alt="ロゴ"></img>
+    <div className='splash'>
+        <Image src="/public/assets/logo.svg" alt="ロゴ" width={350} height={350}/>
     </div>
     );
 };
