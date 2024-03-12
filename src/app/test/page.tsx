d82adf41b41c9862173a9d63fb3401166fd710dd
TKMT <tkmt.takashi02@gmail.com>
2024-03-13 03:07:39 +0900

    ユーザーのリスト

---
+++ src/app/test/page.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { lazy } from 'react';
+import { Dosis } from 'next/font/google';
+import Splash from '@/components/splash';
+import Rooms from '@/components/room';
+import Mainheader from '@/components/main_header';
+import GroupAdd from '@/components/add_group';
+import Mainfooter from '@/components/main_footer';
+import Subheader from '@/components/sub_header';
+import Comment from '@/components/comment';
+import UserInfo from '@/components/user_info';
+
+
+
+const DosisFont = Dosis({
+    weight: "400",
+    subsets: ["latin"]
+})
+
+export default function App() {
+    return (
+        <>
+            <Mainheader />
+            <Splash />
+            <Rooms />
+            <GroupAdd />
+            <Mainfooter />
+            <Subheader />
+            <Comment />
+            <UserInfo />
+        </>
+    )
+}
+
