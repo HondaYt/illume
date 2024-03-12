9a846330ba0eea50052f8276bdb8385bf726ad34
TKMT <tkmt.takashi02@gmail.com>
2024-03-12 18:03:30 +0900

    なんか途中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,23 @@
 "use client"
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import { lazy } from 'react';
+import { Dosis } from 'next/font/google';
+import { waitForDebugger } from 'inspector';
 
-
+const DosisFont = Dosis({
+    weight: "400",
+    subsets: ["latin"]
+})
 
 
 const HomePage = () => {
-    const Router = useRouter();
-
-    // useEffect(() => {
-    // // 2秒後に次のページに遷移
-    // const timeoutId = setTimeout(() => {
-    //     Router.push('/main');
-    // }, 2000);
-
-    // // コンポーネントがアンマウントされた場合にクリーンアップ
-    // return () => clearTimeout(timeoutId);
-    // }, [Router]);
-
-    return (
-    <div className='splash'>
-        <Image src="/public/assets/logo.svg" alt="ロゴ" width={350} height={350}/>
+    return(
+        <div className='splash'>
+        <Image src="splashLogo.svg" alt="ロゴ" width={150} height={150} loading='lazy'/>
+        <h1 className={DosisFont.className}>illuMe</h1>
     </div>
-    );
-};
+)};
 
 export default HomePage;
