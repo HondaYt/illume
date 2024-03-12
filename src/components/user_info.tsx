15e01717690e1f8217ddc6b3c4c430ae3db52d7a
TKMT <tkmt.takashi02@gmail.com>
2024-03-13 03:28:56 +0900

    User_infoの修正

--- src/components/user_info.tsx
+++ src/components/user_info.tsx
@@ -1,7 +1,7 @@
 "use client"
 import styles from '@/styles/userInfo.module.scss';import Image from 'next/image';
 
-export default function subheader() {
+export default function User_info() {
     return (
         <div className={styles.user_area}>
             <Image src='https://picsum.photos/500' width={50} height={50} loading='lazy' alt="アイコン"></Image>
