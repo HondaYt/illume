4c3d172414334a7b3ecd2e389fab270dc079cd78
TKMT <tkmt.takashi02@gmail.com>
2024-03-13 13:42:27 +0900

    修正ver3

--- src/components/profileHeader.tsx
+++ src/components/profileHeader.tsx
@@ -1,23 +1,15 @@
 "use client"
 import styles from '@/styles/profile_header.module.scss';
-import { Dosis } from 'next/font/google';
 import Image from 'next/image';
 import UserInfo from '@/components/userInfo';
 import Link from 'next/link';
-
-const DosisFont = Dosis({
-    weight: "400",
-    subsets: ["latin"]
-})
 
 
 export default function Profile_header() {
     return (
         <div>
             <div className={styles.profile_header}>
-                <Link href="/src/app/roomInner">
-                    <Image src="assets/icon-back.svg" width={25} height={25} alt=''/>
-                </Link>
+                <Link href="/roomInner"><Image src="assets/icon-back.svg" width={30} height={30} alt=''/></Link>
             </div>
             <div className={styles.profile_container}>
                 <UserInfo />
